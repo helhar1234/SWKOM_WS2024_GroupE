39eaa58612626cadb3e9cad5d2bc03fafedd7c19
Helene Harrer <if22b036@technikum-wien.at>
2024-10-18 02:25:36 +0200

    Initial commit of Spring Boot project

---
+++ src/main/java/at/technikum/SWKOM_WS2024_GroupE/SwkomWs2024GroupEApplication.java
@@ -0,0 +1,13 @@
+package at.technikum.SWKOM_WS2024_GroupE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwkomWs2024GroupEApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwkomWs2024GroupEApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/technikum/SWKOM_WS2024_GroupE/SwkomWs2024GroupEApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikum.SWKOM_WS2024_GroupE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwkomWs2024GroupEApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
