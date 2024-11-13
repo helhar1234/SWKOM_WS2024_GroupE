c01020d78f04dc19670e5d9526548a898f7e70bc
Helene Harrer <if22b036@technikum-wien.at>
2024-11-13 16:50:31 +0100

    emty project

--- PaperlessREST/src/main/java/at/technikum/swkom_ws2024_groupe/customExceptions/DocumentNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package at.technikum.swkom_ws2024_groupe.customExceptions;
-
-public class DocumentNotFoundException extends RuntimeException {
-    public DocumentNotFoundException(String message) {
-        super(message);
-    }
-}
--- PaperlessREST/src/main/java/at/technikum/swkom_ws2024_groupe/customExceptions/InvalidFileUploadException.java
+++
@@ -1,7 +0,0 @@
-package at.technikum.swkom_ws2024_groupe.customExceptions;
-
-public class InvalidFileUploadException extends RuntimeException {
-    public InvalidFileUploadException(String message) {
-        super(message);
-    }
-}
